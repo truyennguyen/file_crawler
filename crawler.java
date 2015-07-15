9326ff13eb06eb4c9d470d6cccb079242a505568
Truyen Nguyen <truyen.m.nguyen@gmail.com>
2015-07-15 00:44:32 -0700

    Add function to find .txt files in folder and zip file

---
+++ crawler.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class crawler {
+
+    private static ArrayList<File> arrTxtFiles = new ArrayList<File>();
+
+    private static void findTxtFiles(String path){
+        File dir = new File(path);
+        File[] files = dir.listFiles();
+        for(File file:files){
+            String filename = file.getName();
+            if(filename.endsWith(".txt"))
+                arrTxtFiles.add(file);
+            else if (filename.endsWith(".zip")){
+                String unzipFolder = filename.substring(0, filename.lastIndexOf('.'));
+                unzip(file.getAbsolutePath(), file.getParent());
+                findTxtFiles(file.getParent() + '/' + unzipFolder);
+            }
+            else if(file.isDirectory())
+                findTxtFiles(file.getPath());
+        }
+    }
+    private static void unzip(String zipFile, String destinationFolder) {
+        File directory = new File(destinationFolder);
+
+        // if the output directory doesn't exist, create it
+        if(!directory.exists())
+            directory.mkdirs();
+
+        // buffer for read and write data to file
+        byte[] buffer = new byte[2048];
+
+        try {
+            FileInputStream fInput = new FileInputStream(zipFile);
+            ZipInputStream zipInput = new ZipInputStream(fInput);
+
+            ZipEntry entry = zipInput.getNextEntry();
+
+            while(entry != null){
+                String entryName = entry.getName();
+                File file = new File(destinationFolder + File.separator + entryName);
+
+                System.out.println("Unzip file " + entryName + " to " + file.getAbsolutePath());
+
+                // create the directories of the zip directory
+                if(entry.isDirectory()) {
+                    File newDir = new File(file.getAbsolutePath());
+                    if(!newDir.exists()) {
+                        boolean success = newDir.mkdirs();
+                        if(success == false) {
+                            System.out.println("Problem creating Folder");
+                        }
+                    }
+                }
+                else {
+                    FileOutputStream fOutput = new FileOutputStream(file);
+                    int count = 0;
+                    while ((count = zipInput.read(buffer)) > 0) {
+                        // write 'count' bytes to the file output stream
+                        fOutput.write(buffer, 0, count);
+                    }
+                    fOutput.close();
+                }
+                // close ZipEntry and take the next one
+                zipInput.closeEntry();
+                entry = zipInput.getNextEntry();
+            }
+
+            // close the last ZipEntry
+            zipInput.closeEntry();
+
+            zipInput.close();
+            fInput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        findTxtFiles("/home/nmt/Desktop/files2");
+    }
+}
