36c5e5891e4d0d9d0bacbf25aaf603da52f69d3b
Truyen Nguyen <truyen.m.nguyen@gmail.com>
2015-07-17 09:04:00 -0700

    Add print sorted histogram based on the word counts

--- crawler.java
+++ crawler.java
@@ -2,9 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
